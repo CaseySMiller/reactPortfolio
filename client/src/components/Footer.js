8ce9daaec66defded1796f26933313e85fa74cbe
CaseySMiller <CaseySMiller@yahoo.com>
2022-07-26 20:31:14 -0600

    updated skills and fixed footer date

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,6 @@
 import React from 'react';
-// import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
-//   const location = useLocation();
-//   const navigate = useNavigate();
     return (
         <footer className="footer">
             <section className="footbox1">
