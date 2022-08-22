14aeef1da019b2361739f0ffaadfedf82dc74b4f
CaseySMiller <CaseySMiller@yahoo.com>
2022-08-22 10:07:31 -0600

    added bootcamp accreditation and mdb effects to skills page

--- client/src/pages/Skills.js
+++ client/src/pages/Skills.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Row, Col, Container, Card } from 'react-bootstrap';
+import { MDBRipple, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
 
 //define an array of skills
 const skillList = [
@@ -34,22 +36,53 @@ const Skills = () => {
             <Container>
                 <Row className='justify-content-center m-3'>
                     <Col md={9}>
-                    <Card className='m-2 mb-3 mx-100 shadow'>
-                            <Card.Body>
-                                <Card.Title className="text-center">
-                                    My Skills
-                                </Card.Title>
-                                <Card.Text>
-                                    <ul class="row">
-                                        {skillList.map((skill, i) => {
-                                            return (
-                                                <li key={i} class="col-lg-6">{skill}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <div className='shadow p-0 m-0'>
+                            <Card className='mb-3 mx-100 hover-shadow'>
+                                <Card.Body>
+                                    <Card.Title className="text-center">
+                                        My Skills
+                                    </Card.Title>
+                                    <Card.Text>
+                                        <ul class="row">
+                                            {skillList.map((skill, i) => {
+                                                return (
+                                                    <li key={i} class="col-lg-6">{skill}</li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <div className='shadow p-0 m-0'>
+                            <Card className='mb-3 mx-100 hover-shadow'>
+                                <Card.Body>
+                                    <Card.Title className="text-center">
+                                        Accreditation
+                                    </Card.Title>
+                                    <Card.Text>
+                                        <MDBRow>
+                                            <MDBCol className='text-center'>
+                                                <MDBRipple 
+                                                rippleTag='a'
+                                                target='_blank'
+                                                href='https://www.credly.com/badges/09038804-6c29-4c2a-adb1-cfe6a497646f/public_url'
+                                                className='rounded-circle hover-zoom justify-content-center'
+                                                >
+                                                    <img
+                                                    src='/images/full-stack-coding-boot-camp.png'
+                                                    className='img-fluid'
+                                                    alt='DU Coding Bootcamp Badge'
+                                                    />
+                                                </MDBRipple>
+                                            </MDBCol>
+                                        </MDBRow>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
                     </Col>
                 </Row>
             </Container>
