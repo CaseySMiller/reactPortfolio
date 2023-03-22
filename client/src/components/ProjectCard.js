fbe0734aa962f68844f087f87ffe3b8b347114c7
CaseySMiller <CaseySMiller@yahoo.com>
2023-03-22 12:50:10 -0600

    added hover effects

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -16,39 +16,42 @@ function ProjectMDBCard(project) {
     } = project;
 
     return (
-        <MDBCard className='h-100 shadow'>
-            {image ? (
-                <MDBCardImage variant="top" src={`/images/${image}`} />
-                ) : (
-                <MDBCardImage variant="top" src={placeholderImage} />
-            )}
-            <MDBCardBody style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <div>
-                    <MDBCardTitle>{title}</MDBCardTitle>
-                    <MDBCardText>
-                        {description}
-                    </MDBCardText>
-                </div>
-                <div style={{marginTop: '12px'}}>
-                    <MDBRow className='justify-content-center mx-2'>
-                        <MDBBtn 
-                        variant="secondary" 
-                        className={largeCol ? 'm-2 col-xs-10 col-lg-5 my-MDBbtn' : 'm-2 col-xs-10 my-MDBbtn'}
-                        target='_blank' 
-                        href={deployedSite}>
-                            Deployed app
-                        </MDBBtn>
-                        <MDBBtn 
-                        variant="secondary" 
-                        className={largeCol ? 'm-2 col-xs-10 col-lg-5 my-MDBbtn' : 'm-2 col-xs-10 my-MDBbtn'}
-                        target='_blank' 
-                        href={gitHub}>
-                            GitHub Repo
-                        </MDBBtn>
-                    </MDBRow>
-                </div>
-            </MDBCardBody>
-        </MDBCard>
+
+        <div className='h-100 shadow p-0 m-0'>
+            <MDBCard className='h-100 hover-shadow'>
+                {image ? (
+                    <MDBCardImage variant="top" src={`/images/${image}`} />
+                    ) : (
+                    <MDBCardImage variant="top" src={placeholderImage} />
+                )}
+                <MDBCardBody style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                    <div>
+                        <MDBCardTitle>{title}</MDBCardTitle>
+                        <MDBCardText>
+                            {description}
+                        </MDBCardText>
+                    </div>
+                    <div style={{marginTop: '12px'}}>
+                        <MDBRow className='justify-content-center mx-2'>
+                            <MDBBtn 
+                            variant="secondary" 
+                            className={largeCol ? 'm-2 col-xs-10 col-lg-5 my-button' : 'm-2 col-xs-10 my-button'}
+                            target='_blank' 
+                            href={deployedSite}>
+                                Deployed app
+                            </MDBBtn>
+                            <MDBBtn 
+                            variant="secondary" 
+                            className={largeCol ? 'm-2 col-xs-10 col-lg-5 my-button' : 'm-2 col-xs-10 my-button'}
+                            target='_blank' 
+                            href={gitHub}>
+                                GitHub Repo
+                            </MDBBtn>
+                        </MDBRow>
+                    </div>
+                </MDBCardBody>
+            </MDBCard>
+        </div>
     )
 };
 
