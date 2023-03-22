0276e297601758736747c747a4ba336648763689
CaseySMiller <CaseySMiller@yahoo.com>
2023-03-22 11:17:57 -0600

    refactored main to use only MDB and get rid of dom errors

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Row, Col, Container, Card } from 'react-bootstrap';
-import { MDBBtn, MDBIcon, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBBtn, MDBIcon, MDBRow, MDBCol, MDBContainer, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody } from 'mdb-react-ui-kit';
 
 
 
@@ -8,31 +7,30 @@ const Home = () => {
 
     return (
         <>
-            <Container className='p-2'>
-                <Row className='justify-content-center'>
-                    <Col md={9}>
-                        <div className='shadow p-0 m-0'>
-                            <Card className='mb-3 mx-100 hover-shadow'>
-                                <Card.Body>
-                                    <Card.Title className="text-center">
+            <MDBContainer className='p-2'>
+                <MDBRow className='justify-content-center'>
+                    <MDBCol md={9}>
+                        <div className='shadow p-0 m-0' id="about-me">
+                            <MDBCard className='mb-3 mx-100 hover-shadow'>
+                                <MDBCardBody>
+                                    <MDBCardTitle className="text-center">
                                         About Me
-                                    </Card.Title>
-                                    <Card.Text className='text-center'>
+                                    </MDBCardTitle>
+                                    <MDBCardText className='text-center'>
                                     I am an aspiring developer and a recent graduate of a full stack bootcamp through the <a href="https://bootcamp.du.edu/coding/" target="_blank" rel="noreferrer">University of Denver</a>. I bring a creative, can-do attitude to every project I take on.
                                     <br/>
                                     I am still learning but I have built web front ends as a hobby for decades. Throughout my training I have gained a strong foundation in building a full stack application in Javascript using current good practice and modern techniques.
                                     <br/>
                                     I have encountered many challenges during my education but I loved every minute of it. I thrive in a fast paced, challenging environment and I look forward to all of the new things I will learn as a Junior Developer.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
+                                    </MDBCardText>
+                                </MDBCardBody>
+                            </MDBCard>
                         </div>
-                        <Card className='m-2 mx-100 shadow'>
-                            <Card.Body>
-                                <Card.Title className="text-center">
+                        <MDBCard className='m-2 mx-100 shadow'>
+                            <MDBCardBody>
+                                <MDBCardTitle className="text-center">
                                     Contact and Links
-                                </Card.Title>
-                                <Card.Text>
+                                </MDBCardTitle>
                                     <MDBRow>
                                         <MDBCol lg="4" className='my-1'>
                                             <MDBBtn 
@@ -58,12 +56,11 @@ const Home = () => {
                                             </MDBBtn>
                                         </MDBCol>
                                     </MDBRow>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
         </>
     );
 };
