402a493c00e14d6dd0cb261fe900093f56606f7d
CaseySMiller <CaseySMiller@yahoo.com>
2024-12-02 18:53:23 -0700

    fix deploy to netlify

--- server/server.js
+++ server/server.js
@@ -8,11 +8,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 app.listen(PORT, () => {
     console.log('Server is up!');
 });
 
-//commment to test github
